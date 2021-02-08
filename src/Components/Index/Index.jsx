61ab9a4706c25a43694cc1c6d5eb20e263cf151c
Kevin Penney <kpenney@gmail.com>
2021-02-08 15:21:44 -0800

    working on hero

---
+++ src/Components/Index/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Index = () => {
+   return (
+      <div className='index'>
+         <header>
+            <h1>
+               Build your <span className='red-text'>arsenal</span> of{' '}
+               <span className='red-text'>sports nutrition</span> products
+            </h1>
+         </header>
+      </div>
+   )
+}
+
+export default Index
