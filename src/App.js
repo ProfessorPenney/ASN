0135fc5e7130eee22ab725672400cd64372f58f4
Kevin Penney <kpenney@gmail.com>
2021-02-08 15:21:29 -0800

    working on hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
-import Nav from './Components/NavBar'
+import Nav from './Components/UI/NavBar'
+import Index from './Components/Index/Index'
 
 function App() {
    return (
       <div className='App'>
          <Nav />
+         <Index />
       </div>
    )
 }
