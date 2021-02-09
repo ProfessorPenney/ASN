0cf76cc48d664d8875c148db6fbf0e5472f9d65c
Kevin Penney <kpenney@gmail.com>
2021-02-09 10:18:22 -0800

    split shirt pics and added footer

---
+++ src/Components/UI/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+   return (
+      <footer>
+         <p>
+            Copyright &#169; {new Date().getFullYear()} Arsenal Sports Nutrition - All Rights
+            Reserved
+         </p>
+      </footer>
+   )
+}
+
+export default Footer
