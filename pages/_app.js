51ba59275a18d9906df9294bd561e2239ed03be2
Kevin Penney <kpenney@gmail.com>
2021-03-16 16:22:43 -0700

    login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
+import { Provider } from 'next-auth/client'
+
 import Footer from '../Components/Footer'
 import Meta from '../Components/Meta'
+
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
    return (
       <>
          <Meta />
-         <Component {...pageProps} />
+         <Provider session={pageProps.session}>
+            <Component {...pageProps} />
+         </Provider>
          <Footer />
       </>
    )
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,12 +1,29 @@
+import React from 'react'
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import Nav from '../Components/NavBar'
 
 import loginStyles from './login.module.scss'
 
 const Login = () => {
+   const [session, loading] = useSession()
+
    return (
       <div className={loginStyles.login}>
          <Nav page='login' />
-         <h2>Login?</h2>
+         {!session && (
+            <>
+               Not signed in <br />
+               <button onClick={() => signIn()}>Sign In</button>
+            </>
+         )}
+         {session && (
+            <>
+               Signed in {session.user.email} <br />
+               <div>Now you can access our super secret pages</div>
+               <button onClick={() => signOut()}>Sign Out</button>
+            </>
+         )}
       </div>
    )
 }
