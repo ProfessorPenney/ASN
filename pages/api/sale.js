2f793fa07599249a30e5d550f53062258172cb1a
Kevin Penney <kpenney@gmail.com>
2021-04-26 12:13:35 -0700

    saving checkout progress

--- pages/api/sale.js
+++ pages/api/sale.js
@@ -1,31 +1,48 @@
 const db = require('../../lib/dbconnection')
+const stripe = require('stripe')(process.env.STRIPE_SK)
 
-export default (req, res) => {
+export default async (req, res) => {
    if (req.method !== 'POST')
       res.status(500).json({ message: 'Sorry we only accept POST requests' })
 
-   const { customer, tax, shipping_cost, price, qty, title } = req.body
-   const sql1 = `SELECT * FROM customer WHERE email='kpenney@gmail.com' LIMIT 1;`
+   const items = 1000
+   const calculateOrderAmount = items => {
+      return items
+   }
 
-   db.query(sql1, (err, result) => {
-      if (err) throw err
-      const { customer_id, shipping_address, first_name, last_name } = result[0]
-      console.log(customer_id)
-      console.log(shipping_address)
-      console.log(first_name)
-      console.log(last_name)
-      const sql2 = `INSERT INTO transaction VALUES(${customer_id}, 0, ${shipping_address}, ${tax}, ${shipping_cost}, CURRENT_TIMESTAMP, '${first_name}', '${last_name}');`
-
-      db.query(sql2, (err, result2) => {
-         if (err) {
-            throw err
-         }
-         const sql3 = `INSERT INTO product_sold VALUES(LAST_INSERT_ID(), 0, ${price}, ${qty}, '${title}');`
-
-         db.query(sql3, (err, result3) => {
-            if (err) throw err
-            res.json({ message: 'Success' })
-         })
-      })
+   const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items),
+      currency: 'usd',
+      payment_method_types: ['card'],
+      receipt_email: 'kpenney@gmail.com'
    })
+
+   console.log(paymentIntent)
+
+   res.json({ payment: paymentIntent })
+
+   // const { customer, tax, shipping_cost, price, qty, title } = req.body
+   // const sql1 = `SELECT * FROM customer WHERE email='kpenney@gmail.com' LIMIT 1;`
+
+   // db.query(sql1, (err, result) => {
+   //    if (err) throw err
+   //    const { customer_id, shipping_address, first_name, last_name } = result[0]
+   //    console.log(customer_id)
+   //    console.log(shipping_address)
+   //    console.log(first_name)
+   //    console.log(last_name)
+   //    const sql2 = `INSERT INTO transaction VALUES(${customer_id}, 0, ${shipping_address}, ${tax}, ${shipping_cost}, CURRENT_TIMESTAMP, '${first_name}', '${last_name}');`
+
+   //    db.query(sql2, (err, result2) => {
+   //       if (err) {
+   //          throw err
+   //       }
+   //       const sql3 = `INSERT INTO product_sold VALUES(LAST_INSERT_ID(), 0, ${price}, ${qty}, '${title}');`
+
+   //       db.query(sql3, (err, result3) => {
+   //          if (err) throw err
+   //          res.json({ message: 'Success' })
+   //       })
+   //    })
+   // })
 }
